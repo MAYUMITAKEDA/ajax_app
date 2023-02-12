aaf860bd5cc0b37855997f0f9b6b46db600f2f2a
MAYUMITAKEDA <mt1291092@gmail.com>
2023-02-12 12:00:50 +0900

    AjaxAPPの準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,15 @@
 class PostsController < ApplicationController
 
   def index
-    @posts = Post.all
+    @posts = Post.order(id: "DESC")
   end
 
-  def new
-  end
+  # 不要なのでコメントアウト
+  # def new
+  # end
 
   def create
     Post.create(content: params[:content])
+    redirect to action: :index
   end
 end
