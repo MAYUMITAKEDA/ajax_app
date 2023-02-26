85c4c3c2a75863be71631c249f0ec20027daec88
MAYUMITAKEDA <mt1291092@gmail.com>
2023-02-26 18:15:03 +0900

    リクエストを処理する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,10 @@
 class PostsController < ApplicationController
-
   def index
     @posts = Post.order(id: "DESC")
   end
 
-  # 不要なのでコメントアウト
-  # def new
-  # end
-
   def create
-    Post.create(content: params[:content])
-    redirect to action: :index
+    post = Post.create(content: params[:content])
+    render json:{ post: post }
   end
 end
